b01a7ed79e94861eb1c7e453f8875a382416bbc9
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-17 17:53:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtGallery::Application.config.secret_key_base = 'af6bf80cb94769598cd35de38bdc4c44a2c6eecd6032673329ea79e831618131c9ef12898ba77d8eb60080f277195f93447e72e2142b8bf20ba159de459a3bdf'
